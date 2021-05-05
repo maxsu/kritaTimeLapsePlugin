e2af52de0f6a45cf02047c1b55c577bbb7c4e9a4
Max Suica <max.suica@gmail.com>
2021-05-04 23:12:09 -0700

    Drop Uncommented Code
    Uncommented code makes it difficult to understand current solution. Dropping.

--- timeLapse/timeLapse.py
+++ timeLapse/timeLapse.py
@@ -4,59 +4,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import subprocess
 import os
-"""
-import multiprocessing as mp
-from multiprocessing import Process, Pipe
-
-aD = Krita.activeDocument()
-path = 'C:\\Users\\Gregor\\Desktop\\image.png'
-iO = krita.InfoObject()
-aD.setBatchmode(True)
-print(aD.batchmode())
-
-iO.setProperties({"alpha":True,"compression":5,"forceSRGB":False,"indexed":True,"interlaced":False,"saveSRGBProfile":False,"transparencyFillcolor":[255,255,255]})
-if (aD.exportImage(path,iO)):
-    print("success")
-"""
-
-"""
-_________________________________________________________________
---- this makes krita crash
---- new approach QTimer
---- also look into exporting vs saving than loading and exporting
-_________________________________________________________________
-
-class WorkerThread(QThread):
-    def run():
-        doc = Krita.instance().activeDocument()
-        doc.setBatchmode(True)
-"""
 
 class timeLapseDocker(DockWidget):
-
-
-    """
-    _________________________________________________________
-    --- part of failed multi processing approach see line 119
-    _________________________________________________________
-    timerIn_conn, timerOut_conn = Pipe()
-    iO = None
-    aD = None
-    path = None
-    p = None
-    def rec(in_conn,document,path,io,sT):
-        c = 0
-        while in_conn.recv() == 1:
-            document.exportImage(path+str(c),io)
-            c += 1
-            time.sleep(sT)
-    """
-
-
     def __init__(self):
         super().__init__()
-        #self.f = open("C:\\Users\\Gregor\\Pictures\\testingkritaplugin\\log.txt", "a")
-        #self.f.write("\nplugin initialized")
         self.recState = "notRecording"
         self.fieldPath = QLineEdit()
         self.fieldFFMPath = QLineEdit()
@@ -66,10 +17,8 @@ class timeLapseDocker(DockWidget):
         self.mainWidget = QWidget(self)
         RGBWidget = QWidget(self)
         self.setWidget(self.mainWidget)
-        #vLayout = QVBoxLayout()
         hLayout = QHBoxLayout()
         gLayout = QGridLayout()
-        #fLayout = QFormLayout()
         self.mainWidget.setLayout(gLayout)
         RGBWidget.setLayout(hLayout)
 
@@ -148,49 +97,15 @@ class timeLapseDocker(DockWidget):
         self.mainWidget.layout().addWidget(self.FFMPathLabel,8,0)
         self.mainWidget.layout().addWidget(self.fieldFFMPath,8,1)
 
-
-
-    """
-    _____________________________________________________________________________
-    ---this approach does not work due to problems pickeling certain objects
-    ---new approach: QThread using signals and slots
-
-    ---maybe try using multiprocessing and passing raw pixel data through Pipe(),
-       using pil to render might be a major performance improvement
-       and/or solve the issue with Qtimer not exporting when brush is down
-    _____________________________________________________________________________
-
-        def startRecProcess(self):
-            self.aD = Krita.instance().activeDocument()
-            self.aD.setBatchmode(True)
-            self.iO = krita.InfoObject()
-            self.path = self.fieldPath.text()
-            self.iO.setProperties({"alpha":True,"compression":5,"forceSRGB":False,"indexed":True,"interlaced":False,"saveSRGBProfile":False,"transparencyFillcolor":[255,255,255]})
-            self.p = Process(target=self.rec, args=(self.timerOut_conn,self.aD,self.path,self.iO,10))
-            self.p.start()
-            self.timerIn_conn.send(1)
-
-
-        def stopRec(self):
-            self.timerIn_conn.send(0)
-
-    """
-
-
     def startRec(self):
 
         if (self.recState == "notRecording"):
-            #self.f.write('\ncalled startRec() recstate was notRecording')
             self.recState = "recording"
-            #self.f.write('\nrecState set to recording')
             self.buttonStopRecording.setEnabled(True)
             self.setUiEnabled(False)
             self.buttonStartRecording.setText("Pause")
-            #self.f.write('\n start button text set to pause')
             if not(os.path.isdir(self.fieldPath.text()+"\\"+self.fieldName.text())):
-                #self.f.write('\n dir doesnt exist')
                 os.mkdir(self.fieldPath.text()+"\\"+self.fieldName.text())
-                #self.f.write('\n dir made')
             self.iO = krita.InfoObject()
             self.iO.setProperties({
                 "alpha":self.checkBoxAlpha.isChecked(),
@@ -203,37 +118,25 @@ class timeLapseDocker(DockWidget):
             self.doc = Krita.instance().activeDocument()
             self.doc.setBatchmode(True)
             self.timer.start(self.fieldImageInterval.value()*1000)
-            #self.f.write('\n timer started with: '+str(self.fieldImageInterval.value()*1000))
 
         elif (self.recState == "pause"):
-            #self.f.write('\n called startRec() recstate was pause ')
             self.timer.start(self.fieldImageInterval.value()*1000)
-            #self.f.write('\n timer started with: '+str(self.fieldImageInterval.value()*1000))
             self.buttonStartRecording.setText("Pause")
             self.buttonStopRecording.setEnabled(True)
             self.recState = "recording"
-            #self.f.write('\n recstate set to recording')
         elif (self.recState == "recording"):
-            #self.f.write('\n called startRec() recstate was recording ')
             self.timer.stop()
-            #self.f.write('\n timer stopped')
             self.buttonStartRecording.setText("Resume")
-            #self.f.write('\n start button text set to resume')
             self.buttonStopRecording.setEnabled(False)
             self.recState = "pause"
-            #self.f.write('\n recstate set to pause')
 
 
 
     def stopRec(self):
-        #self.f.write('\n called stopRec()')
         self.recState = "notRecording"
-        #self.f.write('\n recstate set to notRecording')
         self.timer.stop()
-        #self.f.write('\n timer stopped')
         self.saveImage()
         self.saveCounter = 0
-        #self.f.write('\n counter reset to: '+ str(self.saveCounter))
         self.buttonStartRecording.setEnabled(True)
         self.buttonStartRecording.setText("Start")
         self.buttonStopRecording.setEnabled(False)
@@ -262,17 +165,11 @@ class timeLapseDocker(DockWidget):
         self.fieldName.setEnabled(x)
 
     def saveImage(self):
-        #self.f.write('\n saveImage() called')
         counter = self.saveCounter
-        #self.f.write('\n counter= '+ str(self.saveCounter))
         if (self.doc.exportImage(self.fieldPath.text()+"\\"+self.fieldName.text()+f"\\{counter}.png",self.iO)):
             self.saveCounter += 1
 
-
-
     def canvasChanged(self, canvas):
         pass
-
-
 
 Krita.instance().addDockWidgetFactory(DockWidgetFactory("timeLapse", DockWidgetFactoryBase.DockRight, timeLapseDocker))
